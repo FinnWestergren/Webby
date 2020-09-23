5ca91dad2c37c9f9e04bbcca68d7dae398b83d5e
Finn Westergren <fwesterg5@gmail.com>
2020-09-22 22:05:00 -0400

    dickin around v1

--- client/src/sketch/index.ts
+++ client/src/sketch/index.ts
@@ -1,12 +1,36 @@
 import p5 from "p5";
 
 export default function sketch(p: p5): void {
+    const bpm = 60;
+    const cycleTime = 60000/bpm;
+    const startTime = (new Date()).getTime();
 
-	p.setup = function (): void {
-		p.createCanvas(500, 500);
+	p.setup = () => {
+		p.createCanvas(p.windowWidth, p.windowHeight);
 	};
 
-	p.draw = function (): void {
-        p.ellipse(200,200,200,200);
-	};
+	p.draw = () => {
+        const radius = getRadius();
+        p.ellipse(p.mouseX, p.mouseY, radius, radius);
+        drawDebugSection([
+            { key: "radius", value: radius },
+            { key: "cycleTime", value: cycleTime },
+            { key: "ellapsedTime", value: ellapsedTime() }
+        ]);
+    };
+
+    const drawDebugSection = (infoToDisplay: { key: string, value: number | string }[]) => {
+        p.rect(0,0,500,100);
+        infoToDisplay.forEach((data, index) => {
+            p.text(data.key, 10, 20 * (index + 1))
+            p.text(data.value, 100, 20 * (index + 1))
+         })
+    }
+
+    const ellapsedTime = () => (new Date()).getTime() - startTime;
+
+    const getRadius = () => {
+        return (cycleTime * 0.1) - ((ellapsedTime() % cycleTime) * 0.2);
+    }
+    
 }
